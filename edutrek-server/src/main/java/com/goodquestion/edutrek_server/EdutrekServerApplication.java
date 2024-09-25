205a800cd31593b32d0666790ca4a8e11853186a
peshkes <peshkesv@gmail.com>
2024-09-25 23:44:04 +0400

    Initial commit

---
+++ edutrek-server/src/main/java/com/goodquestion/edutrek_server/EdutrekServerApplication.java
@@ -0,0 +1,13 @@
+package com.goodquestion.edutrek_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutrekServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutrekServerApplication.class, args);
+	}
+
+}
---
+++ edutrek-server/src/test/java/com/goodquestion/edutrek_server/EdutrekServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodquestion.edutrek_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutrekServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
